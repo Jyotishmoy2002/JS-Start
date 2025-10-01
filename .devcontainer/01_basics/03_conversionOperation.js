f9f71803c27130a65706df3d147c9eb5181fb5c6
Jyotishmoy2002 <nityanarzary27@gmail.com>
2025-10-01 21:54:30 +0000

    operations

--- .devcontainer/01_basics/03_conversionOperation.js
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -1,25 +1,42 @@
 let score = "hitesh"
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 // "33" => 33
 // "33abc" => NaN
 // true => 1; false => 0
 let isLoggedIn = 1
 let booleanisLoggedIn = Boolean(isLoggedIn)
-console.log(booleanisLoggedIn);
+// console.log(booleanisLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false; "Jyotish" =>true
 
 let someNumber = 33
 let stNumber = String(someNumber)
-console.log(stNumber);
-console.log(typeof stNumber);
+// console.log(stNumber);
+// console.log(typeof stNumber);
+
+/*************Operations**********/
+let val = 3
+let negval = -val
+// console.log(negval);
+let st1 = "hello"
+let st2 = " Jyotish"
+let st3  = st1 + st2
+// console.log(st3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+console.log(+true);
+console.log(+"");
 
 
