def9cae8a11db4053e6a9ba2770c0bbf4234ce61
Jyotishmoy2002 <nityanarzary27@gmail.com>
2025-10-02 20:23:15 +0000

    flow

---
+++ .devcontainer/01_controlflow.js
@@ -0,0 +1,23 @@
+//if else
+if(condition) {
+
+} else if(condition) {
+    
+} else {
+
+}
+// switch case
+switch (key) {
+    case value:
+        
+        break;
+
+    default:
+        break;
+}
+// falsy values
+// false, 0, -0, BigInt 0n, "", null, undefined, NaN
+// truthy values
+// "0",'false'," ", [], {},function(){}
+
+//Nullish Coalescing Operator (??): null undefined 
